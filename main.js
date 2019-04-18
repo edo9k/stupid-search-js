e77f336ba12364d9c071d1773adaca5d89c0f220
edo9k <edox86@gmail.com>
2019-04-18 17:50:29 -0300

    First couple stupid search functions wrote
    There are a bunch of other browser related problems, since this is not
    going to be run in Node, but in browsers.

---
+++ main.js
@@ -0,0 +1,28 @@
+﻿const book = require('./dom-casmurro')
+
+query = w => new RegExp(w, 'gi')
+
+search = s => {
+  const a = Date.now()
+  const indices = []
+  const regex = query(s)
+  let result = null
+
+  while ((result = regex.exec(book.text))) {
+    indices.push(result.index)
+
+    console.log(`
+      ...${book.text.substring(result.index - 50, result.index + 50)}...
+      (encontrado em ${ (Date.now() - a) / 1000 } segundo/s)
+      --- ---
+    `)
+
+  }
+  console.log(` 
+  ----------------------
+  ${indices.length} resultados encotrados. 
+  `)
+
+}
+
+search('mulher')
