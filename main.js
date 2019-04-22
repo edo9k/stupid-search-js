c3c9404b6b505f6f970e056040edc72aceafa239
edo9k <edox86@gmail.com>
2019-04-22 19:52:42 -0300

    Added async functions
    There are still some problems with certain words, specially words that
    have Brazilian extended characters.

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 ﻿const book = require('./dom-casmurro')
 
-query = w => new RegExp(w, 'gi')
+replaceBrChars = async t => t
+  .replace(/[áàãâä]/ui, 'a')
+  .replace(/[éèêë]/ui, 'e')
+  .replace(/[íìîï]/ui, 'i')
+  .replace(/[óòõôö]/ui, 'o')
+  .replace(/[úùûü]/ui, 'u')
+  .replace(/[ç]/ui, 'c')
+//  .replace(/[^a-z0-9]/i, '_')
+//  .replace(/_+/, '_')
 
-search = s => {
+clean = async t => await replaceBrChars(t.toLowerCase())
+
+query = w => new RegExp(w, 'gi')
+  
+search = async (q, s) => {
   const a = Date.now()
   const indices = []
-  const regex = query(s)
+  let regex = await replaceBrChars(q.toLowerCase())
+  regex = query(regex)
   let result = null
 
-  while ((result = regex.exec(book.text))) {
+  while ((result = regex.exec(s))) {
     indices.push(result.index)
 
     console.log(`
@@ -22,7 +35,44 @@ search = s => {
   ----------------------
   ${indices.length} resultados encotrados. 
   `)
-
 }
 
-search('mulher')
+
+(async () => {
+  const rawargs = process.argv
+  const args = rawargs.slice(2)
+  const searchText = await clean(book.text.toLowerCase())
+
+
+  const searchQuery = args
+    .map(x => x.toString().trim())
+    .join(' ')
+
+
+  if (typeof searchQuery === 'string'
+      && searchQuery.trim() !== ''
+      && searchQuery.length > 3 ) {
+    await search( searchQuery, searchText )
+  } else {
+    console.error(`
+      Search queries must be:
+        - String type (text)
+        - Non empty
+        - Longer than 3 characters
+
+      Please try again.
+    `)
+  }
+
+
+  console.log(` 
+      --- ---
+      searchQuery ----------------------  ${ searchQuery}
+      searchQuery sanitized ------------  ${ await replaceBrChars(searchQuery.toLowerCase())}
+      && typeof searchQuery === 'string'  ${ typeof searchQuery === 'string'}
+      && searchQuery.trim() !== '' -----  ${ searchQuery.trim() !== '' }
+      && searchQuery.length > 2 --------  ${ searchQuery.length > 2 }
+  `)
+
+
+})()
